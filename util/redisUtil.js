fc0c8f90aefb4f9599b7e6ba879e7149ad4ea614
dumingcode <jake1026@126.com>
2018-07-26 10:55:27 +0800

    redis disconnet

--- util/redisUtil.js
+++ util/redisUtil.js
@@ -7,24 +7,28 @@ module.exports = {
         let redis = new Redis(redisConfig)
         let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.hget(key, field)
+        await redis.disconnect()
         return result
     },
     async redisHGet(key, field) {
         let redis = new Redis(redisConfig)
         let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.hget(key, field)
+        await redis.disconnect()
         return result
     },
     async redisSet(key, value) {
         let redis = new Redis(redisConfig)
         let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.set(key, value)
+        await redis.disconnect()
         return result
     },
     async redisGet(key) {
         let redis = new Redis(redisConfig)
         let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.get(key)
+        await redis.disconnect()
         return result
     }
 }
