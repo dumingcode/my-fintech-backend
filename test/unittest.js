cff07f0be218f46a1bfa197cd6ce31d1fde7e846
dumingcode <jake1036@126.com>
2019-05-16 18:23:37 +0800

    删除无用test

--- test/unittest.js
+++ test/unittest.js
@@ -5,21 +5,7 @@ const app = require('../app')
 const expect = chai.expect
 const request = supertest(app.listen())
 
-describe('开始测试indexInvest的GET请求', () => {
 
-    it('测试/queryLxrIndexAllData.json请求', (done) => {
-        request
-            .get('/indexInvest/queryLxrIndexAllData.json')
-            .expect(200)
-            .end((err, res) => {
-
-                //   expect(res.body.code).to.be.equal(1)
-                //   expect(res.body.msg).to.be.equal('ok')
-                //expect(res.body.data).to.be.an('object')
-                done()
-            })
-    })
-})
 
 describe('POST /Bigdata Covers', function() {
     it('query sina stock API', function(done) {
@@ -43,35 +29,3 @@ describe('GET /Bigdata Covers', function() {
             });
     });
 });
-
-describe('开始测试indexInvest的GET请求', () => {
-
-    it('测试/queryLxrIndexDealDate.json请求', (done) => {
-        request
-            .get('/indexInvest/queryLxrIndexDealDate.json')
-            .expect(200)
-            .end((err, res) => {
-
-                //   expect(res.body.code).to.be.equal(1)
-                //   expect(res.body.msg).to.be.equal('ok')
-                //expect(res.body.data).to.be.an('object')
-                done()
-            })
-    })
-})
-
-describe('开始测试qmDealDate的GET请求', () => {
-
-    it('测试/queryQmIndexDealDate.json请求', (done) => {
-        request
-            .get('/indexInvest/queryQmIndexDealDate.json')
-            .expect(200)
-            .end((err, res) => {
-
-                //   expect(res.body.code).to.be.equal(1)
-                //   expect(res.body.msg).to.be.equal('ok')
-                //expect(res.body.data).to.be.an('object')
-                done()
-            })
-    })
-})
