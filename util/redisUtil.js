fec12f8d8ff4296a2f1946b0799c37ad243606a6
dumingcode <jake1036@126.com>
2019-06-05 09:44:59 +0800

    规整reids公共包

--- util/redisUtil.js
+++ util/redisUtil.js
@@ -5,31 +5,28 @@ const redisConfig = config.redis
 module.exports = {
     async redisHGet(key, field) {
         let redis = new Redis(redisConfig)
-        let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.hget(key, field)
         await redis.disconnect()
         return result
     },
     async redisSet(key, value) {
         let redis = new Redis(redisConfig)
-        let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.set(key, value)
         await redis.disconnect()
         return result
     },
     async redisGet(key) {
         let redis = new Redis(redisConfig)
-        let authPromise = await redis.auth(redisConfig.password)
         let result = await redis.get(key)
         await redis.disconnect()
         return result
     },
     redisSmembers(key) {
         let redis = new Redis(redisConfig)
-        return redis.auth(redisConfig.password).then((val) => {
-            return redis.smembers(key)
-        }).then((val) => {
+        return redis.smembers(key).then((val) => {
             return val
+        }).then(()=>{
+            redis.disconnect()
         }).catch((err) => {
             console.log(err)
             return null
